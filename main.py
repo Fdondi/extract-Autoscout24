4ed1b23ad1bf742fb86c4fdc805f62291a8b61b6
Francesco Dondi <francesco314@gmail.com>
2023-10-23 20:12:28 +0200

    does not crash, but does not actually catch timeout separately.

--- main.py
+++ main.py
@@ -7,24 +7,56 @@ from random import uniform
 base_url = "https://www.autoscout24.ch"
 
 
-def download(link, page):
-    page.goto(f'{base_url}{link}')
+def try_goto(link, browser, timeout=1):
+    page = browser.new_page()
+    try:
+        page.goto(link)
+    except TimeoutError:
+        page.close()
+        print(f'Timeout on {link}')
+        # Once we hit 1000 seconds, we include an increasing chance to give up 
+        if uniform(0, 1) > 1000/timeout:
+            print('roll failed, giving up')
+            return None
+        print(f'sleeping {timeout}s then retrying')
+        sleep(timeout)
+        return try_goto(link, browser, 2 * timeout)
+    except Exception as ex:
+        print(f'Unknown exception {ex} on {link}, giving up')
+        page.close()
+        return None
+    return page
+
+
+def save_to_file(link, page):
     filename = f'download/{link[6:]}.html'  # remove the language prefix, add folder and extension
     with open(filename, 'w') as f:
         f.write(page.content())
-    sleep(uniform(0.1, 0.3))  # avoid making calls too close by keeping the page open between 0.1 and 3 seconds
-    page.close()
+
+
+def download(link, browser):
+    page = try_goto(f'{base_url}{link}', browser)
+    if page is not None:
+        save_to_file(link, page)
+        page.close()
+    sleep(uniform(0.1, 0.3))  # avoid making calls too close by keeping the page open between 0.1 and 3 seconds    
+
+
+def read_index(i, browser):
+    index_page = try_goto(f'{base_url}/it/automobili/tutte-le-marche?page={i}&vehtyp=10', browser)
+    if index_page is None:
+        print(f'ERROR could not open index page #{i}, skipping')
+        return
+    link_elements = index_page.query_selector_all("article.vehicle-card a.base-a-link")
+    for link_element in link_elements:
+        download(link_element.get_attribute("href"), browser)
+    index_page.close()
 
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)  # Headless seems to trigger bot detection.
-    page = browser.new_page()
     for i in range(1, 7716):
-        page.goto(f'{base_url}/it/automobili/tutte-le-marche?page={i}&vehtyp=10')
-        link_elements = page.query_selector_all("article.vehicle-card a.base-a-link")
-        for link_element in link_elements:
-            link = link_element.get_attribute("href")
-            download(link, browser.new_page())  # This will print each link URL to the console
+        read_index(i, browser)
         # avoid making calls too close by sleeping between 0.5 and 5 seconds
         sleep(uniform(0.5, 5))
     browser.close()
